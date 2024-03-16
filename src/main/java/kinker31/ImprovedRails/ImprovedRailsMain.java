1ddbf3e654dfad2adc71d86222986843d3e5be05
kinker31 <dp223171@gmail.com>
2024-03-15 22:08:09 -0500

    New resources, and basic Olivine Alloy Implementaion. Tools to come later.

--- src/main/java/kinker31/ImprovedRails/ImprovedRailsMain.java
+++ src/main/java/kinker31/ImprovedRails/ImprovedRailsMain.java
@@ -1,9 +1,19 @@
 package kinker31.ImprovedRails;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.block.Block;
+import net.minecraft.core.block.material.Material;
+import net.minecraft.core.block.tag.BlockTags;
+import net.minecraft.core.item.Item;
+import net.minecraft.core.item.material.ArmorMaterial;
 import net.minecraft.core.item.material.ToolMaterial;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import turniplabs.halplibe.helper.ArmorHelper;
+import turniplabs.halplibe.helper.BlockBuilder;
+import turniplabs.halplibe.helper.ItemHelper;
+import turniplabs.halplibe.helper.RecipeBuilder;
+import turniplabs.halplibe.helper.recipeBuilders.RecipeBuilderBlastFurnace;
 import turniplabs.halplibe.util.ConfigHandler;
 import turniplabs.halplibe.util.GameStartEntrypoint;
 import turniplabs.halplibe.util.RecipeEntrypoint;
@@ -39,11 +49,29 @@ public class ImprovedRailsMain implements ModInitializer, GameStartEntrypoint, R
 
 		c.updateConfig();
 	}
-	public static ToolMaterial olivineAlloyTool = new ToolMaterial().setMiningLevel(4);
+	public static ToolMaterial olivineAlloyTool = new ToolMaterial()
+		.setMiningLevel(4)
+		.setDurability(4096)
+		.setDamage(4)
+		.setEfficiency(4f, 2f);
+	public static ArmorMaterial olivineAlloyArmor = ArmorHelper
+		.createArmorMaterial(MOD_ID, "olivinealloy", 2048, 100f, 100f, 100f, 100f);
+	public static Block olivineAlloyBlock = new BlockBuilder(MOD_ID)
+		.setSideTextures("olivinealloyblockside.png")
+		.setTopTexture("olivinealloyblocktop.png")
+		.setBottomTexture("olivinealloyblockbottom.png")
+		.setHardness(5f)
+		.setResistance(2000f)
+		.addTags(BlockTags.MINEABLE_BY_PICKAXE)
+		.build(new Block("block.olivineAlloy", blockID++, Material.metal));
+
+	public static Item rawOlivineAlloy = ItemHelper
+		.createItem(MOD_ID, new Item("raw.olivinealloy", itemID++), "raw_olivine_alloy.png");
+	public static Item ingotOlivineAlloy = ItemHelper
+		.createItem(MOD_ID, new Item("ingot.olivinealloy", itemID++), "olivine_alloy.png");
     @Override
     public void onInitialize() {
 		LOGGER.info("Improved Rails are a go!");
-
     }
 
 	@Override
@@ -58,6 +86,30 @@ public class ImprovedRailsMain implements ModInitializer, GameStartEntrypoint, R
 
 	@Override
 	public void onRecipesReady() {
+		/* Olivine Alloy Process:
+		1. 4 Iron on the corners, 4 Gold on the cardinals, and a Block of Olivine.
+		2. Cook the result of Step 1 on a blast furnace.
+		3. Profit, or in this case, risk and reward!
+		 */
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape(
+				"IGI",
+				"GOG",
+				"IGI")
+			.addInput('I', Item.ingotIron)
+			.addInput('G', Item.ingotGold)
+			.addInput('O', Block.blockOlivine)
+			.create("raw_olivine_alloy", rawOlivineAlloy.getDefaultStack());
+		RecipeBuilder.BlastFurnace(MOD_ID)
+			.setInput(rawOlivineAlloy)
+			.create("olivine_alloy", ingotOlivineAlloy.getDefaultStack());
 
+		RecipeBuilder.Shaped(MOD_ID)
+			.setShape(
+				"OOO",
+				"OOO",
+				"OOO")
+			.addInput('O', ingotOlivineAlloy)
+			.create("block_of_olivine_alloy", olivineAlloyBlock.getDefaultStack());
 	}
 }
